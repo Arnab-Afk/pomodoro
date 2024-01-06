bfe80e376d19dd3229c88debfb45d5d8c6070a14
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-01-06 11:00:03 +0530

    glassmorphism on settings modal

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -1,31 +1,32 @@
 // SettingsModal.jsx
 import React from 'react';
+import '../index.css';
 
 const SettingsModal = ({ isOpen, onClose, onThemeChange, themes, currentTheme }) => {
   if (!isOpen) return null;
 
+  const handleSelectWorld = (theme) => {
+    onThemeChange(theme); // Assuming onThemeChange expects an object with theme details
+    onClose(); // Close the modal after selection
+  };
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
-      <div className="bg-white p-8 rounded-lg shadow-lg space-y-4">
-        <h2 className="text-xl font-bold">Settings</h2>
-        <div>
-          <label htmlFor="theme-select" className="block text-lg">Select theme:</label>
-          <select
-            id="theme-select"
-            value={currentTheme}
-            onChange={onThemeChange}
-            className="mt-1 block w-full"
-          >
-            {themes.map(theme => (
-              <option key={theme.name} value={theme.name}>{theme.name}</option>
-            ))}
-          </select>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="p-8 rounded-lg shadow-lg space-y-4 glassmorphism">
+        <h2 className="text-xl font-bold text-white">Pick your Ambient Mode world</h2>
+        <div className="grid-container">
+          {themes.map((theme) => (
+            <div
+              key={theme.name}
+              className={`grid-item ${currentTheme.name === theme.name ? 'selected' : ''}`}
+              onClick={() => handleSelectWorld(theme)}
+            >
+              <img src={theme.image} alt={theme.name} className="grid-image" />
+              <div className="grid-item-name">{theme.name}</div>
+            </div>
+          ))}
         </div>
-        <div className="flex justify-end space-x-4">
-          <button onClick={onClose} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full shadow-md transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-300">
-            Close
-          </button>
-        </div>
+        <button onClick={onClose} className="save-button">Save</button>
       </div>
     </div>
   );
---
+++ src/fonts/cinzel.jsx
@@ -0,0 +1,4 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Cinzel&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200')
+</style>
