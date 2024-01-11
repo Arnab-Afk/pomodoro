736624fedc117255377b8bc83b243b6c43a61fd7
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-01-11 14:47:34 +0530

    Update SettingsModal.jsx

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -9,6 +9,33 @@ const SettingsModal = ({ isOpen, onClose, onThemeChange, themes, currentTheme })
     onThemeChange(theme); // Assuming onThemeChange expects an object with theme details
     onClose(); // Close the modal after selection
   };
+const items = [
+    {
+      id: 1,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 2,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 3,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 4,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 5,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+  ];
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
@@ -27,6 +54,34 @@ const SettingsModal = ({ isOpen, onClose, onThemeChange, themes, currentTheme })
           ))}
         </div>
         <button onClick={onClose} className="save-button">Save</button>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-5'>
+            {items.map(({ id, src, link }) => (
+              <div
+                key={id}
+                className='shadow-md shadow-gray-600 rounded-lg overflow-hidden'
+              >
+                <img
+                  src={src}
+                  alt=''
+                  className='rounded-md duration-200 hover:scale-105'
+                />
+                <div className='flex items-center justify-center'>
+                  <button
+                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+                    onClick={() => window.open(link, '_blank')}
+                  >
+                    button
+                  </button>
+                  <button
+                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+                    onClick={() => window.open(link, '_blank')}
+                  >
+                    button
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
       </div>
     </div>
   );
