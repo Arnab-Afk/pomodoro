21d8188d817b59d643db5fb5fa500d28e9dceae7
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-01-11 15:16:57 +0530

    Update SettingsModal.jsx

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -56,9 +56,9 @@ const items = [
         <button onClick={onClose} className="save-button">Save</button>
         <div
         name=''
-        className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen text-center md:text-left'
+        className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen text-center md:text-left '
       >
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full glassmorphism'>
           <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
 Title            </p>
