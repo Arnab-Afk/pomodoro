4bd9dfe3dffcaaeacabd92b125d207baa1bfe483
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-01-11 15:06:58 +0530

    Update SettingsModal.jsx

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -77,18 +77,8 @@ Title            </p>
                   className='rounded-md duration-200 hover:scale-105'
                 />
                 <div className='flex items-center justify-center'>
-                  <button
-                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
-                    onClick={() => window.open(link, '_blank')}
-                  >
-                    button
-                  </button>
-                  <button
-                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
-                    onClick={() => window.open(link, '_blank')}
-                  >
-                    button
-                  </button>
+                  
+                 
                 </div>
               </div>
             ))}
