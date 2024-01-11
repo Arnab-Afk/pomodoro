f7a4737ab45a45fbef5389fcc9d05caf8031d063
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-01-11 15:18:38 +0530

    Update SettingsModal.jsx

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -59,11 +59,7 @@ const items = [
         className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen text-center md:text-left '
       >
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full glassmorphism'>
-          <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
-Title            </p>
-            <p className='py-6'>subtitle</p>
-          </div>
+          
 
           <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-5'>
             {items.map(({ id, src, link }) => (
